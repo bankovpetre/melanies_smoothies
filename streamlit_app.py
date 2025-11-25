08a094ab358bb26c6429f3164a844cef03d28c07
Peter Bankov <bankovpetre@gmail.com>
2025-11-25 00:55:33 -0800

    Update streamlit_app.py
    add new column in the select

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ st.write(
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.stop()
 
 ingredients_list = st.multiselect(
     'Chose up to five ingredients:',
